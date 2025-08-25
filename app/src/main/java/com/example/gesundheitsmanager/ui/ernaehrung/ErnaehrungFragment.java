cf9d05a727180016d548929f944c4016442aff71
unknown <Kirolossarwat11@gmail.com>
2025-08-25 22:22:56 +0200

    Add new fruits with some information as text

--- app/src/main/java/com/example/gesundheitsmanager/ui/ernaehrung/ErnaehrungFragment.java
+++ app/src/main/java/com/example/gesundheitsmanager/ui/ernaehrung/ErnaehrungFragment.java
@@ -5,11 +5,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.navigation.Navigation;
 import com.example.gesundheitsmanager.R;
 
 public class ErnaehrungFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ernaehrung, container, false);
+        View view = inflater.inflate(R.layout.fragment_ernaehrung, container, false);
+
+        Button buttonBanane = view.findViewById(R.id.button_banane);
+        Button buttonApfel = view.findViewById(R.id.button_apfel);
+        Button buttonWassermelone = view.findViewById(R.id.button_wassermelone);
+        Button buttonOrange = view.findViewById(R.id.button_orange);
+        Button buttonErdbeere = view.findViewById(R.id.button_erdbeere);
+
+        buttonBanane.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.fragment_banane));
+        buttonApfel.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.fragment_apfel));
+        buttonWassermelone.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.fragment_wassermelone));
+        buttonOrange.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.fragment_orange));
+        buttonErdbeere.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.fragment_erdbeere));
+
+        return view;
     }
 }
